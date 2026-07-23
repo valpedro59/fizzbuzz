c76a0f24160cc94f5af40a608fe37a5f51a75a4b
valpedro59 <valpedro59@gmail.com>
2026-07-23 19:29:47 +0100

    adatping program with ui

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,22 @@
+// Le pont JS -> page. À recopier tel quel dans chaque projet S7 à S9.
+const sortie = document.querySelector("#sortie");
+function afficher(html) {
+  sortie.innerHTML += html;
+}
 // fizzbuzz function
 function fizzBuzz() {
+  const fizzMsg = `<p class="fizz">Fizz</p>`;
+  const buzzMsg = `<p class="buzz">Buzz</p>`;
+  const fizzBuzzMsg = `<p class="fizzbuzz">FizzBuzz</p>`;
   let i = 1;
   while (i <= 100) {
     // Cas spécial : divided by 3 and by 5
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
+      afficher(fizzBuzzMsg);
     } else if (i % 3 === 0) {
-      console.log("Fizz");
+      afficher(fizzMsg);
     } else if (i % 5 === 0) {
-      console.log("Buzz");
+      afficher(buzzMsg);
     } else {
       // If the number is not divided by 3 or by 5
       console.log(i);
--- table.js
+++ table.js
@@ -1,40 +1,58 @@
+const sortieTab = document.querySelector("#tab");
+function afficher(html) {
+  sortieTab.innerHTML += html;
+}
+const multiTab = document.querySelector("#multitab");
+function afficherMulti(html) {
+  multiTab.innerHTML += html;
+}
+
 // table de multiplication
 function tableMultiplication(num) {
   // Initialition du multiplicateur
   let i = 1;
   //   la valeur de la derniere multiplication
-  let answer;
+  let answer = 0;
   while (i <= 10) {
     // Multiplication du nombre fois l'indice
     answer = num * i;
     // Affichage de chaque ligne de table
-    console.log(`${num} fois ${i} font : ${answer}`);
+    // console.log(`${num} fois ${i} font : ${answer}`);
+    // variable output table
+    const showTabOfNumber = `<p class='tab-num'>${num} fois ${i} = <strong>${answer}</strong></p>`;
+    afficher(showTabOfNumber);
     // Iteration pour passer a l'indice suivant
     i++;
   }
   return answer;
 }
 // Test de la fonction avec le nombre 5
-// console.log(tableMultiplication(5));
+tableMultiplication(5);
 
-// Functionc to generate the table 1 to 9
+// CORRECTION DE VOTRE FONCTION MULTITABLE
 function multiTable() {
-  // init of var answer
   let answer = 0;
 
-  //  loop to define table from 1 to 9
+  // Boucle principale de 1 à 9
   for (let i = 1; i < 10; i++) {
-    console.log(`Debut de la table de multiplication de ${i}`);
-    for (
-      let numberToMutltiply = 1;
-      numberToMutltiply < 11;
-      numberToMutltiply++
-    ) {
-      answer = i * numberToMutltiply;
-      console.log(`${i} * ${numberToMutltiply} = ${answer}`);
+    // 1. On commence à construire la carte pour la table en cours
+    let carteHTML = `<div class="carte-table">`;
+    carteHTML += `<h4>Table de ${i}</h4>`;
+
+    // Boucle interne de 1 à 10 pour générer les lignes de calcul
+    for (let numberToMultiply = 1; numberToMultiply < 11; numberToMultiply++) {
+      answer = i * numberToMultiply;
+      // On ajoute chaque ligne de calcul à l'intérieur de la carte
+      carteHTML += `<p>${i} x ${numberToMultiply} = <strong>${answer}</strong></p>`;
     }
-    console.log(`fin de la table de multiplication de ${i}\n`);
+
+    // 2. On ferme la balise de la carte
+    carteHTML += `</div>`;
+
+    // 3. On injecte la carte complète dans le conteneur HTML
+    afficherMulti(carteHTML);
   }
 }
 
+// Exécution de la fonction
 multiTable();
